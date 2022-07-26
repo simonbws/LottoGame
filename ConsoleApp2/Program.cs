178138b08d6e63829c1323cfcb14acce02e73be9
Szymon Bywalec <simonbws6@gmail.com>
2022-07-26 21:24:12 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LottoGame
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+        }
+    }
+}
